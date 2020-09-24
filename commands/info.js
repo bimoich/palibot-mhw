8fbfb595d4eee57dfab4970642961e5f6ed0ec46
bimoich <myaxenation49@gmail.com>
2020-09-24 15:29:00 +0700

    add additional config data, test some implementation

--- commands/info.js
+++ commands/info.js
@@ -1,5 +1,7 @@
-const fetch = require('node-fetch');
-const Discord = require('discord.js');
+import fetch from 'node-fetch'
+import Discord from 'discord.js'
+// const fetch = require('node-fetch');
+// const Discord = require('discord.js');
 var levenshtein = require('fast-levenshtein');
 const { elements, ailments, monster_list, locations, blights } = require('../ressources/config.json');
 const { src_thumbnail } = require('../ressources/src_thb.json')
